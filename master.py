731fcaee304a0d532e1685408f62ef93c93c7cdf
WarBean <zhhuab123456@gmail.com>
2017-05-31 18:14:00 +0800

    init

---
+++ master.py
@@ -0,0 +1,167 @@
+import re
+import time
+import json
+import itchat
+import argparse
+from threading import Thread, Lock
+from http.server import HTTPServer
+from http.server import BaseHTTPRequestHandler
+
+class CustomHandler(BaseHTTPRequestHandler):
+    inform_record = { }
+
+    def do_GET(self):
+        length = int(self.headers['content-length'])
+        info = json.loads(self.rfile.read(length).decode())
+        slaver_address, _ = self.client_address
+        lock.acquire()
+        info_record[slaver_address] = info
+        lock.release()
+        inform_waste(info, self.inform_record)
+        self.send_response(200)
+        self.end_headers()
+
+    def log_message(self, format, *args):
+        return
+
+def http_func():
+    server = HTTPServer((opt.address, opt.port), CustomHandler)
+    print("监听服务开启，按<Ctrl-C>退出")
+    server.serve_forever()
+
+def inform_condition(mem_usage, gpu_percent, cpu_percent, wechatname, pid, inform_record):
+    if mem_usage < 1000: return False
+    if cpu_percent > 5 and gpu_percent > 5: return False
+    curr_time = time.time()
+    if (wechatname, pid) not in inform_record:
+        inform_record[(wechatname, pid)] = curr_time
+        return True
+    if curr_time - inform_record[(wechatname, pid)] > 30:
+        inform_record[(wechatname, pid)] = curr_time
+        return True
+    return False
+
+def inform_waste(info, inform_record):
+    for slaver_address in sorted(info_record.keys()):
+        gi_list = info_record[slaver_address]['gpu']
+        pi_list = info_record[slaver_address]['process']
+        for pi in pi_list:
+            gi = gi_list[pi['gpuid']]
+            if inform_condition(pi['mem_usage'], gi['percent'], pi['cpu_percent'], pi['wechatname'], pi['pid'], inform_record):
+                friend = itchat.search_friends(nickName = pi['wechatname'])
+                if len(friend) == 0:
+                    print('不存在微信好友：<%s>' % pi['wechatname'])
+                    continue
+                information = [
+                    '检测到程序长时间高内存消耗且低负载空转：',
+                    '所在服务器：%s' % slaver_address,
+                    'GPU id：%d' % pi['gpuid'],
+                    'PID：%d' % pi['pid'],
+                    '程序名：%s' % pi['program'],
+                    '进程GPU内存占用：%dM' % pi['mem_usage'],
+                    '所在GPU内存占用：%dM/%dM' % (gi['mem_usage'], gi['mem_total']),
+                    '所在GPU使用率：%d%%' % gi['percent'],
+                    '进程CPU使用率：%d%%' % pi['cpu_percent'],
+                ]
+                friend[0].send('\n'.join(information))
+                print('向<%s>发送通知：\n\t%s' % (pi['wechatname'], '\n\t'.join(information)))
+
+def report_server():
+    report = ['服务器列表：']
+    lock.acquire()
+    for slaver_address in sorted(info_record.keys()):
+        report.append(slaver_address)
+    lock.release()
+    report = '\n'.join(report)
+    return report
+
+def report_gpu(slaver_address = None):
+    report = []
+    lock.acquire()
+    if slaver_address is None:
+        address_list = sorted(info_record.keys())
+    else:
+        address_list = [slaver_address]
+    for slaver_address in address_list:
+        report.append('服务器地址: %s' % slaver_address)
+        gi_list = info_record[slaver_address]['gpu']
+        pi_list = info_record[slaver_address]['process']
+        for gpuid, gi in enumerate(gi_list):
+            report.append('GPU%d 显存%dM/%dM 使用率%d%%' % (
+                gpuid, int(gi['mem_usage']), int(gi['mem_total']), int(gi['percent'])
+            ))
+        report.append('进程列表')
+        for pi in pi_list:
+            report.append('GPU%d %s %s 显存%dM CPU占比%d%%' % (
+                pi['gpuid'], pi['username'], pi['wechatname'], int(pi['mem_usage']), int(pi['cpu_percent'])
+            ))
+        report.append('=' * 10)
+    if report != []: del report[-1]
+    lock.release()
+    report = '\n'.join(report)
+    return report
+
+def report_user():
+    usage_dict = { }
+    lock.acquire()
+    for slaver_address in sorted(info_record.keys()):
+        pi_list = info_record[slaver_address]['process']
+        for pi in pi_list:
+            wechatname = pi['wechatname']
+            mem_usage = pi['mem_usage']
+            usage_dict[wechatname] = usage_dict.get(wechatname, 0) + mem_usage
+    lock.release()
+    usage_list = sorted(usage_dict.items(), key = lambda x: x[1])
+    report = ['用户显存占用排序：'] + ['%s : %dM' % (n, u) for n, u in usage_list]
+    report = '\n'.join(report)
+    return report
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--address', required = True, help = 'master服务器IP地址')
+parser.add_argument('--port', type = int, default = '5678', help = 'master服务器端口，默认5678')
+parser.add_argument('--interval', type = int, default = '1800', help = '通知间隔时间，默认1800秒')
+opt = parser.parse_args()
+
+info_record = { }
+lock = Lock()
+
+http_thread = Thread(target = http_func)
+http_thread.setDaemon(True)
+http_thread.start()
+
+@itchat.msg_register(itchat.content.TEXT)
+def receive_text(msg):
+    print('收到指令: %s' % msg.text)
+    error = '\n'.join([
+        '请使用有效指令：',
+        'user：查看用户使用情况',
+        'server：查看服务器列表',
+        'gpu：查看所有GPU使用情况',
+        'gpu <完整IP地址或后缀>：查看指定服务器GPU使用情况',
+    ])
+    if msg.text == 'server':
+        return report_server()
+    if msg.text.startswith('gpu'):
+        if msg.text == 'gpu':
+            return report_gpu()
+        tokens = msg.text.split()
+        if len(tokens) != 2:
+            return error
+        slaver_address = tokens[1]
+        if not re.fullmatch('\d+\.\d+\.\d+\.\d+', slaver_address):
+            candidates = [add for add in info_record.keys() if add.endswith(slaver_address)]
+            if len(candidates) == 0:
+                return '服务器%s不存在' % slaver_address
+            if len(candidates) > 1:
+                report = ['请指明是以下哪个服务器：'] + candidates
+                return '\n'.join(report)
+            slaver_address = candidates[0]
+        if slaver_address not in info_record:
+            return '服务器%s不存在' % slaver_address
+        return report_gpu(slaver_address)
+    if msg.text in ['用户', 'user']:
+        return report_user()
+    return error
+
+itchat.auto_login(enableCmdQR = 2, hotReload = True)
+itchat.run()
---
+++ slaver.py
@@ -0,0 +1,117 @@
+import re
+import pwd
+import time
+import json
+import psutil
+import argparse
+import requests
+import subprocess
+
+def get_owner(pid):
+    try:
+        for line in open('/proc/%d/status' % pid):
+            if line.startswith('Uid:'):
+                uid = int(line.split()[1])
+                return pwd.getpwuid(uid).pw_name
+    except:
+        return None
+
+def get_info():
+    info = { 'gpu': [], 'process': [] }
+    msg = subprocess.Popen('nvidia-smi', stdout = subprocess.PIPE).stdout.read().decode()
+    msg = msg.strip().split('\n')
+
+    lino = 8
+    while True:
+        status = re.findall('.*\d+%.*\d+C.*\d+W / +\d+W.* +(\d+)MiB / +(\d+)MiB.* +(\d+)%.*', msg[lino])
+        if status == []: break
+        mem_usage, mem_total, percent = status[0]
+        info['gpu'].append({
+            'mem_usage': float(mem_usage),
+            'mem_total': float(mem_total),
+            'percent': float(percent),
+        })
+        lino += 3
+
+    lino = -1
+    while True:
+        lino -= 1
+        status = re.findall('\| +(\d+) +(\d+) +\w+ +([^ ]*) +(\d+)MiB \|', msg[lino])
+        if status == []: break
+        gpuid, pid, program, mem_usage = status[0]
+        username = get_owner(int(pid))
+        if username is None:
+            print('进程已经不存在1')
+            continue
+        wechatname = name_dict.get(username, username)
+        try:
+            p = psutil.Process(int(pid))
+            p.cpu_percent()
+            time.sleep(0.5)
+            cpu_percent = p.cpu_percent()
+        except psutil.NoSuchProcess:
+            print('进程已经不存在2')
+            continue
+        info['process'].append({
+            'gpuid': int(gpuid),
+            'pid': int(pid),
+            'program': program,
+            'cpu_percent': cpu_percent,
+            'mem_usage': float(mem_usage),
+            'username': username,
+            'wechatname': wechatname,
+        })
+    info['process'].reverse()
+
+    return info
+
+def running_mean(mean_info, curr_info, decay):
+    def merge(a, b): return a * decay + b * (1 - decay)
+    new_info = { 'gpu': [], 'process': [] }
+    for mean_gi, curr_gi in zip(mean_info['gpu'], curr_info['gpu']):
+        new_info['gpu'].append({
+            'mem_usage': merge(mean_gi['mem_usage'], curr_gi['mem_usage']),
+            'mem_total': merge(mean_gi['mem_total'], curr_gi['mem_total']),
+            'percent': merge(mean_gi['percent'], curr_gi['percent']),
+        })
+    mean_pi_dict = { (pi['gpuid'], pi['pid'], pi['program'], pi['username']): pi for pi in mean_info['process'] }
+    curr_pi_dict = { (pi['gpuid'], pi['pid'], pi['program'], pi['username']): pi for pi in curr_info['process'] }
+    mean_pi_keys = set(mean_pi_dict.keys())
+    curr_pi_keys = set(curr_pi_dict.keys())
+    for key in sorted(set.union(mean_pi_keys, curr_pi_keys)):
+        if key in mean_pi_keys and key in curr_pi_keys:
+            mean_pi = mean_pi_dict[key]
+            curr_pi = curr_pi_dict[key]
+            mean_pi['mem_usage'] = merge(mean_pi['mem_usage'], curr_pi['mem_usage'])
+            mean_pi['cpu_percent'] = merge(mean_pi['cpu_percent'], curr_pi['cpu_percent'])
+            new_info['process'].append(mean_pi)
+        elif key not in mean_pi_keys:
+            curr_pi = curr_pi_dict[key]
+            new_info['process'].append(curr_pi)
+    return new_info
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--address', required = True, help = 'master服务器IP地址')
+parser.add_argument('--port', default = '5678', help = 'master服务器端口，默认5678')
+opt = parser.parse_args()
+
+url = 'http://%s:%s' % (opt.address, opt.port)
+name_dict = dict([
+    line.strip().split()
+    for line in open('username_to_wechatname.txt', encoding = 'utf8')
+])
+mean_info = None
+
+while True:
+    curr_info = get_info()
+    if mean_info is None:
+        mean_info = curr_info
+    else:
+        mean_info = running_mean(mean_info, curr_info, 0.9)
+    data = json.dumps(mean_info)
+    try:
+        response = requests.get(url, data = data)
+        print('HTTP状态码:', response.status_code)
+    except Exception as e:
+        print(e)
+    time.sleep(1)
